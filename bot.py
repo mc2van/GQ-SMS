289cb0ba262223850309f3d9beee0e977bb8ae8e
Evan Qi <evanqi@gmail.com>
2021-01-06 18:52:49 -0500

    dir change

--- bot.py
+++ bot.py
@@ -12,10 +12,10 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
-model = load_model('chatbot_model.h5')
-intents = json.loads(open('intents.json').read())
-words = pickle.load(open('words.pkl', 'rb'))
-classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('/training/chatbot_model.h5')
+intents = json.loads(open('/training/intents.json').read())
+words = pickle.load(open('/training/words.pkl', 'rb'))
+classes = pickle.load(open('/training/classes.pkl', 'rb'))
 app = Flask(__name__)
 
 
