bc61a955df65c5e91e3242e7ea2aacf6fa3f373d
Evan Qi <evanqi@gmail.com>
2021-01-10 02:05:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,96 +1,96 @@
-# import random
-# import json
-# from keras.models import load_model
-# import numpy as np
-# import pickle
-# from flask import Flask, request
-# import twilio
-# from twilio import twiml
-# from twilio.twiml.messaging_response import MessagingResponse
-# import requests
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
+import random
+import json
+from keras.models import load_model
+import numpy as np
+import pickle
+from flask import Flask, request
+import twilio
+from twilio import twiml
+from twilio.twiml.messaging_response import MessagingResponse
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# import nltk
-# from nltk.stem import WordNetLemmatizer
-# lemmatizer = WordNetLemmatizer()
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
-# model = load_model('chatbot_model.h5')
-# intents = json.loads(open('intents.json').read())
-# words = pickle.load(open('words.pkl', 'rb'))
-# classes = pickle.load(open('classes.pkl', 'rb'))
-# app = Flask(__name__)
-
-
-# @app.route('/bot', methods=['POST'])
-# def bot():
-#     incomingMsg = request.values.get('Body', '').lower()
-#     resp = MessagingResponse()
-#     msg = resp.message()
-#     msg.body(chatbot_response(incomingMsg))
-#     return str(resp)
+model = load_model('chatbot_model.h5')
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+app = Flask(__name__)
 
 
-# def clean_up_sentence(sentence):
-#     # tokenize the pattern - split words into array
-#     sentence_words = nltk.word_tokenize(sentence)
-#     # stem each word - create short form for word
-#     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-#     return sentence_words
-# # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+@app.route('/bot', methods=['POST'])
+def bot():
+    incomingMsg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    msg.body(chatbot_response(incomingMsg))
+    return str(resp)
 
 
-# def bow(sentence, words, show_details=True):
-#     # tokenize the pattern
-#     sentence_words = clean_up_sentence(sentence)
-#     # bag of words - matrix of N words, vocabulary matrix
-#     bag = [0]*len(words)
-#     for s in sentence_words:
-#         for i, w in enumerate(words):
-#             if w == s:
-#                 # assign 1 if current word is in the vocabulary position
-#                 bag[i] = 1
-#                 if show_details:
-#                     print("found in bag: %s" % w)
-#     return(np.array(bag))
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
 
-# def predict_class(sentence, model):
-#     # filter out predictions below a threshold
-#     p = bow(sentence, words, show_details=False)
-#     res = model.predict(np.array([p]))[0]
-#     ERROR_THRESHOLD = 0.25
-#     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-#     # sort by strength of probability
-#     results.sort(key=lambda x: x[1], reverse=True)
-#     print(results)
-#     return_list = []
-#     for r in results:
-#         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-#     return return_list
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print("found in bag: %s" % w)
+    return(np.array(bag))
 
 
-# def getResponse(ints, intents_json):
-#     tag = ints[0]['intent']
-#     list_of_intents = intents_json['intents']
-#     for i in list_of_intents:
-#         if(i['tag'] == tag):
-#             result = random.choice(i['responses'])
-#             break
-#     return result
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    print(results)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
 
 
-# def chatbot_response(text):
-#     ints = predict_class(text, model)
-#     if float(ints[0]['probability']) < 0.5:
-#         return "Sorry, I’m not fully equipped to respond to that. Type ‘prompts’ for some possible prompts, or try asking me something else."
-#     res = getResponse(ints, intents)
-#     return res
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag'] == tag):
+            result = random.choice(i['responses'])
+            break
+    return result
 
-# @app.route('/')
-# def index():
-#     return "<h1>Hello</h1>"
 
-# if __name__ == "__main__":
-#     app.run(port=5000)
+def chatbot_response(text):
+    ints = predict_class(text, model)
+    if float(ints[0]['probability']) < 0.5:
+        return "Sorry, I’m not fully equipped to respond to that. Type ‘prompts’ for some possible prompts, or try asking me something else."
+    res = getResponse(ints, intents)
+    return res
+
+@app.route('/')
+def index():
+    return "<h1>Hello</h1>"
+
+if __name__ == "__main__":
+    app.run(port=5000)
