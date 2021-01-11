2fc300d0695663b2a5b3671a8c54cd5eddee2a33
Evan Qi <evanqi@gmail.com>
2021-01-10 20:58:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -93,4 +93,5 @@ def index():
     return "<h1>Hello</h1>"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
